ef524ce6daba3516950717f3dc0465049f87e127
Alejandra Quelali <alejandra.day.123@gmail.com>
2021-06-28 17:01:44 -0400

    Added thing to do component

--- src/components/HomeSearchBar.vue
+++ src/components/HomeSearchBar.vue
@@ -1,17 +1,18 @@
 <template>
   <v-container class="primary lighten-5 rounded-lg py-4">
     <v-row>
-      <v-col class="ml-6" cols="auto" sm="3"> 
-        <v-text-field class="ink--text text--darken-4 caption"
-            label="Ubicacion"
-            prepend-icon="mdi-map-marker"
+      <v-col class="ml-6" cols="auto" sm="3">
+        <v-text-field
+          class="ink--text text--darken-4 caption"
+          label="Ubicacion"
+          prepend-icon="mdi-map-marker"
         ></v-text-field>
       </v-col>
       <v-col cols="auto" sm="3">
         <v-menu
           v-model="dateMenu"
           :close-on-content-click="false"
-          :nudge-right="40" 
+          :nudge-right="40"
           transition="scale-transition"
           offset-y
           full-width
@@ -28,14 +29,19 @@
               v-on="on"
             ></v-text-field>
           </template>
-          <v-date-picker locale="en-in" v-model="arrival" no-title @input="dateMenu = false"></v-date-picker>
+          <v-date-picker
+            locale="en-in"
+            v-model="arrival"
+            no-title
+            @input="dateMenu = false"
+          ></v-date-picker>
         </v-menu>
       </v-col>
-     <v-col cols="auto" sm="3">
+      <v-col cols="auto" sm="3">
         <v-menu
           v-model="dateMenu2"
           :close-on-content-click="false"
-          :nudge-right="40" 
+          :nudge-right="40"
           transition="scale-transition"
           offset-y
           full-width
@@ -51,17 +57,17 @@
               v-on="on"
             ></v-text-field>
           </template>
-          <v-date-picker locale="en-in" v-model="departure" no-title @input="dateMenu2 = false"></v-date-picker>
+          <v-date-picker
+            locale="en-in"
+            v-model="departure"
+            no-title
+            @input="dateMenu2 = false"
+          ></v-date-picker>
         </v-menu>
       </v-col>
       <v-col class="pl-8 pr-6 mr-0" cols="auto">
-        <v-btn class="primary darken-2 white--text"
-          fab
-          large
-        >
-          <v-icon dark>
-            mdi-magnify
-          </v-icon>
+        <v-btn class="primary darken-2 white--text" fab large>
+          <v-icon dark> mdi-magnify </v-icon>
         </v-btn>
       </v-col>
     </v-row>
@@ -69,10 +75,10 @@
 </template>
 
 <style>
-  .container {
-    width: 616px;
-    height: 96px;
-  }
+.container {
+  width: 616px;
+  height: 96px;
+}
 </style>
 
 <script>
@@ -83,7 +89,7 @@ export default {
       dateMenu: false,
       dateMenu2: false,
       arrival: null,
-      departure: null
+      departure: null,
     };
   },
 };
